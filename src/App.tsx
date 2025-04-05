74a6df599c28ec71cb13129b9df6fc18f89a3472
Dinarchak <jjustdinar@gmail.com>
2025-04-05 19:51:20 +0300

    Удалил лишние импорты и добавил компонент главной страницы и страницу пользователя

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,11 @@ import RegisterPage from './components/Pages/Register/register';
 import MainLayout from './components/Layouts/MainLayout/MainLayout';
 import UserHomePage from './components/Pages/UserHomePage/UserHomePage';
 import ProtectedRoute from './components/Layouts/ProtectedRoute';
-import userService from './services/userService';
+import MainPage from './components/Pages/MainPage/MainPage';
 
 import { createBrowserRouter, RouterProvider } from 'react-router';
-import Avatar from './components/UI/Avatar/avatar';
 import AuthProvider from './provider/authProvider';
-import { h } from 'react-router/dist/development/fog-of-war-BaM-ohjc';
+
 
 const router = createBrowserRouter([
   {
@@ -24,27 +23,13 @@ const router = createBrowserRouter([
       },
       {
         path: '/',
-        element: <h1>Главная страница</h1>
+        Component: MainPage
       },
       {
         Component: ProtectedRoute,
         children: [
           {
             path: 'profile',
-            // возвращает data, соотвествующий интерфейсу User
-            loader: async ({params}) => {
-              // const data = await userService.getUser(Number(params.user_id))
-              // return {data}
-    
-              return ({
-                id: 0,
-                email: 'jjustdinar@yandex.ru',
-                full_name: 'Фазлиев Динар Данилевич',
-                type: 'Студент',
-                avatar: 'https://oimages.anime-pictures.net/da0/da0679b2552d7a4b211b3319c78a2550.jpg?if=ANIME-PICTURES.NET_-_863442-6387x3382-wuthering+waves-cantarella+%28wuthering+waves%29-weishiji-single-long+hair-looking+at+viewer.jpg',
-              });
-    
-            },
             Component: UserHomePage
           }
         ]

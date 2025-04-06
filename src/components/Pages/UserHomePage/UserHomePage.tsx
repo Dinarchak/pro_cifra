88505267c62939d1c1fea9156eb707eea482eec1
Dinarchak <jjustdinar@gmail.com>
2025-04-07 01:46:14 +0300

    Обновил верстку главной страницы под случай, если пользователь является ментором(может создавать курсы межвузового обмена)

--- src/components/Pages/UserHomePage/UserHomePage.tsx
+++ src/components/Pages/UserHomePage/UserHomePage.tsx
@@ -2,38 +2,53 @@ import userShownFieldNames from "./constants";
 
 import ObjectFields from "../../Dummies/ObjectFields/ObjectFields";
 import ObjectLabel from "../../Dummies/ObjectLabel/ObjectLabel";
+import CourseForm from "../../Widgets/CreateCourseForm/CreateCourseFrom";
 import Button from "../../UI/Button/Button";
+import CourseCardList from "../../Widgets/CourseCardList/CourseCardList";
 
 import { useEffect , useState } from "react";
 import userService from "../../../services/userService";
 import { useAuth } from "../../../provider/authProvider";
 
 import User from "../../../models/user";
+import courseService from "../../../services/courseService";
+import Course from "../../../models/course";
 
 export default function UserHomePage() {
 
-    const [data, setData] = useState<User>({email: "", fullname: "", type: null});
+    const [user, setUser] = useState<User>({email: "", fullname: "", role: null});
+    const [coursesList, setCoursesList] = useState<Array<Course>>([]);
     const token = useAuth();
 
     useEffect(() => {
       const loadData = async () => {
         try {
           const data = await userService.getUser();
-          setData(data);
+          const coursesList_ = await courseService.getAllCoursesByUser();
+          setUser(data);
+          setCoursesList(coursesList_);
         } catch (error) {
-          console.error("Ошибка при загрузке:", error);
+          console.error("Ошибка при загрузке");
         }
       };
     
       loadData();
-    }, []);
+    });
+
 
     return (
         <>
-          <ObjectLabel avatar="" label={data.fullname}/>
+          <ObjectLabel avatar="" label={user.fullname}/>
           <div>
-            <ObjectFields dataNames={userShownFieldNames} dataValues={data}/>
+            <ObjectFields dataNames={userShownFieldNames} dataValues={user}/>
           </div>
+          {user.role === 'mentor' ? 
+          <>
+            <CourseForm/>
+            <CourseCardList list={coursesList}/>
+          </>
+          : <></>}
+        
           <Button callback={(e) => token.setToken(null)}>Выйти</Button>
         </>
     );

15516b6a837d1c5bd5807c2d90c9b70e302f297d
Dinarchak <jjustdinar@gmail.com>
2025-04-07 01:23:45 +0300

    обновил url-ы для courseService

--- src/services/courseService.tsx
+++ src/services/courseService.tsx
@@ -3,17 +3,17 @@ import { AddCourse } from "../models/course";
 
 const courseService = {
     getAllCourses: async () => {
-        const resp = await api.get('/secured/allCourse');
+        const resp = await api.get('/course/allCourse');
         return resp.data;
     },
 
     getAllCoursesByUser: async () => {
-        const resp = await api.get('/secured/allCourseByUser');
+        const resp = await api.get('/course/allCourseByUser');
         return resp.data;
     },
 
     addCourse: async (data: AddCourse) => {
-        const resp = await api.post('/addCourse/')
+        const resp = await api.post('/course/addCourse', data);
     }
 }
 

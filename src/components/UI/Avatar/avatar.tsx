11d3519d6d9d85f639b083106e5d684bc59365f3
Dinarchak <jjustdinar@gmail.com>
2025-04-08 22:17:33 +0300

    Сделал аватарки варьируемыми в размере и изменил стили

--- src/components/UI/Avatar/avatar.tsx
+++ src/components/UI/Avatar/avatar.tsx
@@ -1,12 +1,15 @@
 import style from "./.module.css";
 
 type AvatarType = {
-    image_path: string;
+    image_path: string,
+    size: number
 }
 
-export default function Avatar({image_path}:AvatarType) {
+export default function Avatar({image_path, size=4}:AvatarType) {
+    const logo_size = size.toString() + "rem";
+
     return (
-    <div className={style.container}>
+    <div className={style.container} style={{height: logo_size, width: logo_size}}>
         <img className={style.avatar} src={image_path}/>
     </div>
     );

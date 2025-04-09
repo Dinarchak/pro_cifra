6caabc5cb69611cc38ccb4da4b73c26c8a7e5d9f
Dinarchak <jjustdinar@gmail.com>
2025-04-10 00:49:55 +0300

    refactor: удалил неиспользуемые импорты

--- src/components/Pages/MainPage/MainPage.tsx
+++ src/components/Pages/MainPage/MainPage.tsx
@@ -1,8 +1,6 @@
-import styles from "./.module.css";
 import { useEffect, useState } from "react";
 import Course from "../../../models/course";
 import courseService from "../../../services/courseService";
-import CourseCard from "../../Dummies/CourseCard/CourseCard";
 import CourseCardList from "../../Widgets/CourseCardList/CourseCardList";
 
 export default function MainPage() {

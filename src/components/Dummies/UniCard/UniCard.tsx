bdc84b8a077519afb879b965e86c049456bde5e2
Dinarchak <jjustdinar@gmail.com>
2025-04-11 02:09:51 +0300

    feature: сверстать карточку вуза в более презентабильный вид
    Добавлены аватарка, убрана запятая в конце перечисления направлений и изменена верстка

--- src/components/Dummies/UniCard/UniCard.tsx
+++ src/components/Dummies/UniCard/UniCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import University from "../../../models/university";
 import Avatar from "../../UI/Avatar/avatar";
 import style from "./.module.css";
@@ -19,29 +20,29 @@ type UniCardType = {
 export default function UniCard({obj}: UniCardType) {
 
     let courses_str = "";
-    for (let i = 0; i < Math.min(3, uni.giveCourseDTOList.length); ++i) {
-        courses_str += `${uni.giveCourseDTOList[i].major}, `;
+    for (let i = 0; i < Math.min(3, obj.giveCourseDTOList.length); ++i) {
+        courses_str += `${obj.giveCourseDTOList[i].major}, `;
     }
 
-    if (uni.giveCourseDTOList.length > 3) {
-        courses_str += `и еще ${uni.giveCourseDTOList.length - 3} ${get_correct_form(uni.giveCourseDTOList.length - 3)} программы обмена`;
+    courses_str = courses_str.slice(0, courses_str.length - 2);
+
+    if (obj.giveCourseDTOList.length > 3) {
+        courses_str += `и еще ${obj.giveCourseDTOList.length - 3} ${get_correct_form(obj.giveCourseDTOList.length - 3)} программы обмена`;
     }
 
 
     return <>
         <div className={style.card}>
-            <Avatar image_path="https://avatars.mds.yandex.net/i?id=45379510be7254a216b9b148c628954943ca18e0-2455126-images-thumbs&n=13" size={4}/>
+            <div className={style.uniAvatar}>
+                <Avatar image_path="https://avatars.mds.yandex.net/i?id=45379510be7254a216b9b148c628954943ca18e0-2455126-images-thumbs&n=13" size={4}/>
+            </div>
             <div className={style.desc}>
-                <h3 className={style.name}>{uni.university}</h3>
+                <Link to={`/university/${obj.id}`}><h3 className={style.name}>{obj.university}</h3></Link>
                 <p className={style.courses}>{courses_str}</p>
             </div>
             <div className={style.photo}>
-
+                <img src="https://avatars.mds.yandex.net/i?id=c14545ffcb5e55a037d9972947ec2f38a3fab30d-12482695-images-thumbs&n=13"/>
             </div>
         </div>
     </>;
-}
-
-function min(arg0: number) {
-    throw new Error("Function not implemented.");
 }

8eb424b806a45413e4e5b0842a49596d0498679d
Dinarchak <jjustdinar@gmail.com>
2025-04-11 02:10:39 +0300

    feature: загрузить аватарку пользователя на карточке и привязать ссылку на его профиль

--- src/components/Dummies/UserProfileLink/UserProfileLink.tsx
+++ src/components/Dummies/UserProfileLink/UserProfileLink.tsx
@@ -1,5 +1,9 @@
+import { useEffect, useState } from "react";
+import userService from "../../../services/userService";
+import Avatar from "../../UI/Avatar/avatar";
 import style from "./.module.css";
 import { Link } from "react-router";
+import default_avatar from "../../../static/user-svgrepo-com.svg";
 
 type UserProfileLinkType = {
     id: number,
@@ -9,12 +13,27 @@ type UserProfileLinkType = {
 }
 
 export default function UserProfileLink({id, name, email, avatar}: UserProfileLinkType) {
+
+    const [avatarUrl, setAvatarUrl] = useState("");
+
+    useEffect(() => {
+        const loadData = async () => {
+            const avatar = await userService.getUserAvatar(id);
+            const avatarUrl_ = URL.createObjectURL(avatar);
+            setAvatarUrl(avatarUrl_);
+        }
+    })
+
+    
     return <>
         <div className={style.container}>
-            <Link to={`/user/${id}`}>
-                <h3 className={style.title}>{name}</h3>
-            </Link>
-            <p className={style.subtitle}>{email}</p>
+            <Avatar size={5} image_path={avatarUrl === "" ? default_avatar : avatarUrl}/>
+            <div className={style.bio}>
+                <Link to={`/user/${id}`}>
+                    <h3 className={style.title}>{name}</h3>
+                </Link>
+                <p className={style.subtitle}>{email}</p>
+            </div>
         </div>
     </>;
 }

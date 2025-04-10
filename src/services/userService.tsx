45c724aa78f7c732e274de02acfa6c8a1de6d9a0
Dinarchak <jjustdinar@gmail.com>
2025-04-11 02:14:54 +0300

    feature: добавить запрос аватарки пользователя

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -4,6 +4,11 @@ const userService = {
     getUser: async() => {
         const resp = await api.get('/user/getAllAboutUser');
         return resp.data;
+    },
+
+    getUserAvatar: async(id: number) => {
+        const resp = await api.get(`/photo/user/${id}`, {responseType: 'blob'});
+        return resp.data;
     }
 };
 

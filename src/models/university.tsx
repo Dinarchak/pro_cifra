95de7626f01d23906efdccb0a17e16ce3a30af29
Dinarchak <jjustdinar@gmail.com>
2025-04-11 02:13:17 +0300

    refactor: добавить поле id в интерфейс вуза

--- src/models/university.tsx
+++ src/models/university.tsx
@@ -2,6 +2,7 @@ import Course from "./course";
 import User from "./user";
 
 export default interface University {
+    id: number,
     university: string,
     giveCourseDTOList: Array<Course>,
     giveUserDTOList: Array<User>

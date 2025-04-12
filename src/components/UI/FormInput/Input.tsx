f0437a2d777eb178cd5c95d313aa97b142acf436
Dinarchak <jjustdinar@gmail.com>
2025-04-12 19:07:23 +0300

    feature: добавил поля для плейсхолдера и значения по умолчанию

--- src/components/UI/FormInput/Input.tsx
+++ src/components/UI/FormInput/Input.tsx
@@ -2,11 +2,13 @@ import styles from "./.module.css"
 
 type FormInputType = {
   label: string,
-  type: string
+  type: string,
+  value?: string,
+  placeholder?: string,
   callback: (value: string) => void;
 };
 
-export default function FormInput({label, callback, type}: FormInputType) {
+export default function FormInput({label, callback, type, value, placeholder}: FormInputType) {
 
     const listener = (e: React.ChangeEvent<HTMLInputElement>) => {
       callback(e.target.value);
@@ -15,6 +17,8 @@ export default function FormInput({label, callback, type}: FormInputType) {
     return <div className={styles.inputGroup}>
               <label>{label}</label>
               <input
+                value={value}
+                placeholder={placeholder}
                 type={type}
                 required
                 onChange={listener}/>

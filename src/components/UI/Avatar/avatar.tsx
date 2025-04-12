3e8f02fec9921e3f7bffe2105d854370a3143227
Dinarchak <jjustdinar@gmail.com>
2025-04-12 23:50:33 +0300

    refactor: убрать ненужный вывод в консоль

--- src/components/UI/Avatar/avatar.tsx
+++ src/components/UI/Avatar/avatar.tsx
@@ -7,9 +7,6 @@ type AvatarType = {
 
 export default function Avatar({blob, size=4}:AvatarType) {
     const logo_size = size.toString() + "rem";
-
-    console.log(blob);
-
     return (
     <div className={style.container} style={{height: logo_size, width: logo_size}}>
         {

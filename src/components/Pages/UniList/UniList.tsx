438b8d813687f78528f7835cca8aca6a006436ba
Dinarchak <jjustdinar@gmail.com>
2025-04-12 19:08:10 +0300

    feature: добавил простой фильтр к списку университетов

--- src/components/Pages/UniList/UniList.tsx
+++ src/components/Pages/UniList/UniList.tsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import uniService from "../../../services/uniService";
 import University from "../../../models/university";
 import CardList from "../../Widgets/CardList/CardList";
 import UniCard from "../../Dummies/UniCard/UniCard";
+import FilterInput from "../../Widgets/Filter/FilterInput";
 
 
 export default function UniList() {
 
   const [uniList, setUniList] = useState<Array<University>>([]);
+  const [filter, setFilter] = useState("");
+
   useEffect(() => {
       const loadData = async () => {
           try {
@@ -20,6 +23,15 @@ export default function UniList() {
         loadData();
   });
 
-    return <><CardList<University> list={uniList} Card={UniCard}/></>;
+  const filteredList = useMemo(() => {
+    const res = uniList.filter(uni => uni.university.toLowerCase().includes(filter.toLowerCase()))
+    return res;
+  }, [filter, uniList])
+
+
+    return <>
+        <FilterInput filter={filter} onFilterChange={setFilter}/>
+        <CardList<University> list={filteredList} Card={UniCard}/>
+      </>;
 }
 

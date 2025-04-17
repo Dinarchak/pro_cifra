57365e6debee18fbe5b21d75e82bcd31c672ecf5
Dinarchak <jjustdinar@gmail.com>
2025-04-17 21:17:58 +0300

    feature: удаление программ обмена

--- src/components/Dummies/CourseCard/CourseCard.tsx
+++ src/components/Dummies/CourseCard/CourseCard.tsx
@@ -1,16 +1,26 @@
 import Course from "../../../models/course";
 import styles from "./.module.css";
-
+import bin from "../../../static/delete.svg"
 import ObjectFields from "../ObjectFields/ObjectFields";
 import ProgramCardFieldNames from "./constants"
 import { textMaxLength } from "./constants";
 import { Link } from "react-router-dom";
+import { Button, Modal } from "react-bootstrap";
+import AddMentorForm from "../../Widgets/AddMentorForm/AddMentorForm";
+import { useState } from "react";
+import courseService from "../../../services/courseService";
 
 type CourseCardType = {
     obj: Course;
+    candelete?: boolean
 }
 
-export default function CourseCard({obj}: CourseCardType) {
+export default function CourseCard({obj, candelete=false}: CourseCardType) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => {setShow(false)};
+    const handleShow = () => {setShow(true)};
+
     const objCopy = {...obj};
     if (objCopy.description.length > textMaxLength) {
         objCopy.description = objCopy.description.slice(0, textMaxLength) + "...";
@@ -19,11 +29,26 @@ export default function CourseCard({obj}: CourseCardType) {
     if (objCopy.requirement.length > textMaxLength) {
         objCopy.requirement = objCopy.requirement.slice(0, textMaxLength) + "...";
     }
-
     return (<>
         <div className={styles.card + " bodyText"}>
             <Link to={`/course/${objCopy.id}`}><h3 className={styles.cardName}>{objCopy.major}</h3></Link>
             <ObjectFields dataValues={objCopy} dataNames={ProgramCardFieldNames}/>
+            {candelete && <button onClick={handleShow} className={styles.deleteBtn}><img src={bin}/></button>}
         </div>
+
+        <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Удаление программы обмена</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>Вы точно хотите удалить программу обмена?</Modal.Body>
+              <Modal.Footer>
+                <Button onClick={async () => {
+                    await courseService.deleteCourse(obj.id);
+                    handleClose();
+                }}>Да</Button>
+                <Button onClick={handleClose}>Нет</Button>
+              </Modal.Footer>
+        </Modal> : <></>
+
     </>);
 }
--- src/components/Widgets/CardList/CardList.tsx
+++ src/components/Widgets/CardList/CardList.tsx
@@ -1,14 +1,17 @@
 import styles from "./.module.css";
 
-type CardListType<T extends {id: number}> = {
-    list: Array<T>,
-    Card: React.ComponentType<{obj: T}>;
-}
-
-export default function CardList<T extends {id: number}>({list, Card}: CardListType<T>) {
+type CardListType<
+  T extends { id: number },
+  ExtraProps = any
+> = {
+  list: Array<T>;
+  Card: React.ComponentType<{ obj: T } & ExtraProps>;
+  cardProps?: ExtraProps
+};
+export default function CardList<T extends {id: number}, ExtraProps>({list, Card, cardProps}: CardListType<T, ExtraProps>) {
     const coursesList = list.map(obj => <>
         <li key={obj.id} className={styles.courseCard}>
-            <Card obj={obj}/>
+            <Card {...({ obj, ...cardProps } as { obj: T } & ExtraProps)}/>
         </li>
     </>)
 

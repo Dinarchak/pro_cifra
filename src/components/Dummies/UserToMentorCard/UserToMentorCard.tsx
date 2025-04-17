98d42aa232ac9c5a7f589a5c6f3b95d0f6bcb109
Dinarchak <jjustdinar@gmail.com>
2025-04-17 21:19:36 +0300

    fix: пользователь не исчезает из списка после назначения оного ментором

--- src/components/Dummies/UserToMentorCard/UserToMentorCard.tsx
+++ src/components/Dummies/UserToMentorCard/UserToMentorCard.tsx
@@ -8,24 +8,30 @@ import plus from "../../../static/add.svg"
 import mentorService from "../../../services/mentorService";
 import default_avatar from "../../../static/user-svgrepo-com.svg";
 
+type UserToMentorCardType = {
+    user: User,
+    callback?: () => Promise<void>
+}
 
-export default function UserToMentorCard({user}: {user: User}) {
+export default function UserToMentorCard(props: UserToMentorCardType) {
 
     const [avatar, setAvatarBlob] = useState<Blob>();
 
+    const default_callback = async () => {
+        await mentorService.setUserAsMentor(props.user.id)
+    }
+
     const fetchData = useCallback(async () => {
-        setAvatarBlob(await userService.getUserAvatar(user.id))
-    }, [user.id])
+        setAvatarBlob(await userService.getUserAvatar(props.user.id))
+    }, [props.user.id])
 
     usePooling(10000, fetchData)
 
     return <div className={styles.container}>
         <div className={styles.label}>
             <Avatar size={3} blob={avatar} enabled={false} default_avatar={default_avatar}/>
-            <p>{user.fullname}</p>
+            <p>{props.user.fullname}</p>
         </div>
-        <button className={styles.doMentorBtn} onClick={async (e) => {
-            console.log("click")
-            await mentorService.setUserAsMentor(user.id)}}><img src={plus}/></button>
+        <button className={styles.doMentorBtn} onClick={props.callback ? props.callback : default_callback}><img src={plus}/></button>
     </div>
 }
--- src/components/Widgets/AddMentorForm/AddMentorForm.tsx
+++ src/components/Widgets/AddMentorForm/AddMentorForm.tsx
@@ -4,17 +4,18 @@ import FormInput from "../../UI/FormInput/Input";
 import User from "../../../models/user";
 import UserToMentorCard from "../../Dummies/UserToMentorCard/UserToMentorCard";
 import styles from "./.module.css";
+import mentorService from "../../../services/mentorService";
 
 export default function AddMentorForm() {
     const [fullname, setFullname] = useState("");
     const [userList, setUserList] = useState<Array<User>>([])
+    const [rerender, trigger] = useState(false);
 
     useEffect(() => {
         const loadData = async () => {
             try {
                 if (fullname.length > 0) {
                     const data = await userService.getUsersByFullname(fullname);
-                    console.log(data)
                     setUserList(data)
                 }
             } catch(error) {
@@ -24,7 +25,7 @@ export default function AddMentorForm() {
         }
 
         loadData();
-    }, [fullname]);
+    }, [fullname, rerender]);
 
     const filteredList = useMemo(() => {
         return userList.filter((u) => u.role === null);
@@ -34,7 +35,10 @@ export default function AddMentorForm() {
             <FormInput value={fullname} callback={setFullname} type="text" placeholder="Введите ФИО"/>
             <ul className={styles.list}>
                 {filteredList.map(item => <li key={item.id}>
-                    <UserToMentorCard user={item}/>
+                    <UserToMentorCard user={item} callback={async () => {
+                        await mentorService.setUserAsMentor(item.id);
+                        trigger(!rerender);
+                    }}/>
                 </li>)}
             </ul>
         </>
